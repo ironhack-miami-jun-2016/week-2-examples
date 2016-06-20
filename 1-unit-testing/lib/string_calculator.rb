95851faf2195b769a6ab192db77465eacf9e15fd
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2016-06-20 11:00:07 -0400

    Unit Testing: Add dumb solution to StringCalculator

--- 1-unit-testing/lib/string_calculator.rb
+++ 1-unit-testing/lib/string_calculator.rb
@@ -2,5 +2,12 @@
 
 class StringCalculator
   def add(numbers_string)
+    chars = numbers_string.split(",")
+
+    #            0    1
+    #            |    |
+    # chars = [ "5", "7" ]
+
+    chars[0].to_i + chars[1].to_i
   end
 end
